cecaeed62872e668f1d63c826b20592e17f5ab57
IKamuI <32035469+IKamuI@users.noreply.github.com>
2017-10-18 16:30:51 -0200

    criação

---
+++ Estoque/Estoque/Fornecedor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estoque
+{
+    class Fornecedor:PessoaABS
+    {
+        private string cnpj;
+        private string nascimento;
+
+        protected Fornecedor()
+        {
+            cnpj = "default";
+            nascimento = "default";
+        }
+
+       }
+    }
+
+  
---
+++ Estoque/Estoque/PessoaABS.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estoque
+{
+     abstract class PessoaABS
+    {
+        private int codigo;
+        private string nome;
+        private string email;
+
+
+        protected PessoaABS()
+        {
+            codigo = 0;
+            nome = "default";
+            email = "dafault";
+        }
+
+    }
+}
